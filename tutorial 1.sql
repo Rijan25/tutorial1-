878fe58e1746786ae97b2c0a1181f3c74911c458
Rijan25 <rijan07pokhrel@gmail.com>
2023-06-13 19:14:43 +0545

    Finally Completed

--- tutorial 1.sql
+++ tutorial 1.sql
@@ -28,7 +28,7 @@ CREATE TABLE
         FOREIGN KEY (employee_name) REFERENCES tbl_Employee(employee_name),
         manager_name VARCHAR(255)
     );
-
+	INSERT INTO tbl_Employee VALUES ('John Doe','235 Fifth Ave','New York');
 INSERT INTO
     tbl_Employee (employee_name, street, city)
 VALUES (
@@ -55,7 +55,11 @@ VALUES (
         'Michael Brown',
         '902 Main St',
         'Houston'
-    ), (
+    ),(
+        'John Doe',
+        '235 Fifth Ave',
+        'New York'
+    ),(
         'Samantha Smith',
         '111 Second St',
         'Chicago'
@@ -80,6 +84,8 @@ VALUES (
         'Pongyang Corporation',
         500000
     );
+
+
 
 INSERT INTO
     tbl_Works (
@@ -166,13 +172,12 @@ SELECT * FROM tbl_Company;
 
 --- Find all employees in the database who live in the same cities and on the same streets as do their managers.
 
- Select tbl_employee.employee_name from tbl_employee, tbl_Works ,tbl_Company
-   where tbl_Manages.employee_name=tbl_Mangages.employee_name 
-   and tbl_works.Company_name = tbl_Company.Company_name 
-   and tbl_employee.city=tbl_.city;
+ Select tbl_employee.employee_name from tbl_employee, tbl_Works ,tbl_Company,tbl_Manages
+   where tbl_Manages.employee_name=tbl_employee.employee_name and tbl_Manages.manager_name=tbl_Works.employee_name
+   and tbl_employee.street = tbl_Company.Company_name 
+   and tbl_employee.city=tbl_Company.city;
 
-
-
+   
 
 --- Find all employees in the database who do not work for First Bank Corporation.
 
@@ -180,9 +185,15 @@ SELECT * FROM tbl_Company;
 
 --- Find all employees in the database who earn more than each employee of Small Bank Corporation.
 
+   SELECT employee_name FROM tbl_Works WHERE salary > all (SELECT salary FROM tbl_Works WHERE company_name = 'Small Bank Corporation')
+
 --- Assume that the companies may be located in several cities. Find all companies located in every city in which Small Bank Corporation is located.
 
+   SELECT company_name FROM tbl_Company WHERE city= all(SELECT city FROM tbl_Company WHERE city='Chicago')
+
 --- Find all employees who earn more than the average salary of all employees of their company.
+   SELECT employee_name FROM tbl_Works,tbl_Company WHERE salary>(SELECT AVG(salary) FROM tbl_Works WHERE tbl_Works.company_name=tbl_Company.company_name=)
+
 
 --- Find the company that has the most employees.
               SELECT company_name,count(*) as cnt 
@@ -198,7 +209,7 @@ SELECT * FROM tbl_Company;
 
 --- Find those companies whose employees earn a higher salary, on average, than the average salary at First Bank Corporation.
 
-
+        
 
 
 
@@ -250,3 +261,16 @@ WHERE EXISTS
   WHERE salary > 30000);
 
 
+  ---JOIN operation
+
+  SELECT * FROM tbl_Employee LEFT JOIN tbl_Works
+  ON 
+  tbl_Employee.employee_name=tbl_Works.employee_name;
+
+  ---CROSS JOIN operation 
+   
+   SELECT * FROM tbl_Employee CROSS JOIN tbl_Works;
+
+   ---LEFT JOIN 
+   SELECT * FROM tbl_Employee LEFT JOIN tbl_Works
+   ON tbl_Employee.employee_name=tbl_Works.employee_name WHERE company_name ='First Bank Corporation';
